df2fbb60317845091edfc70f425baae479d2d235
your_name <shawnlambert004@gmail.com>
2024-03-10 03:11:57 +0000

    RETURN

--- app/api/frame/.route.ts
+++ app/api/frame/.route.ts
@@ -14,15 +14,16 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmNcVSBegwkWcXfjeSuyrnLtpqfrAG1BERxTAJKbN8V667/6.png.png" />
     <meta property="fc:frame:button:1" content="Back" />
     "fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${prevId}" />
-    <meta property="fc:frame:button:2" content="Follow" />
+    <meta property="fc:frame:button:2" content="idk" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
   </head></html>`);
   } else {
   return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
-    <meta property="fc:frame" content="vNext" /> 
+    <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmNcVSBegwkWcXfjeSuyrnLtpqfrAG1BERxTAJKbN8V667/${id}.png.png" />
     <meta property="fc:frame:button:1" content="Next Page" />
+    <meta property="fc:frame:button:2" content="Follow" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
   </head></html>`);
   }
