3e667414cff017f0c686d4677b0af25339fe4064
your_name <shawnlambert004@gmail.com>
2024-03-10 01:15:36 +0000

    hi

---
+++ app/api/frame/.route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const searchParams = req.nextUrl.searchParams
+  const id:any = searchParams.get("id")
+  const idAsNumber = parseInt(id)
+
+  const nextId = idAsNumber + 1
+  const prevId = idAsNumber - 1
+
+  if(idAsNumber === 7){
+      return new NextResponse(`<!DOCTYPE html><html><head>
+    <title>This is frame 7</title>
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmNcVSBegwkWcXfjeSuyrnLtpqfrAG1BERxTAJKbN8V667/6.png.png" />
+    <meta property="fc:frame:button:1" content="Back" />
+    "fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${prevId}" />
+    <meta property="fc:frame:button:2" content="Follow" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
+  </head></html>`);
+  } else {
+  return new NextResponse(`<!DOCTYPE html><html><head>
+    <title>This is frame ${id}</title>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmNcVSBegwkWcXfjeSuyrnLtpqfrAG1BERxTAJKbN8V667/${id}.png.png" />
+    <meta property="fc:frame:button:2" content="Follow" />
+    <meta property="fc:frame:button:1" content="Next Page" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
+  </head></html>`);
+  }
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "Next"
+    }
+  ],
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmNcVSBegwkWcXfjeSuyrnLtpqfrAG1BERxTAJKbN8V667/0.png.png`,
+  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
+});
+
+export const metadata: Metadata = {
+  title: 'Aleph Zero',
+  description: 'idrc',
+  openGraph: {
+    title: 'Aleph Zero',
+    description: 'IDRC',
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmNcVSBegwkWcXfjeSuyrnLtpqfrAG1BERxTAJKbN8V667/0.png.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Mirsha Promotions</h1>
+    </>
+  );
+}
